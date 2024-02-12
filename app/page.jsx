4968832219b3bec6158e5b021f606c4e99ada523
santohack <santohack238@gmail.com>
2024-02-13 05:02:53 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={72}
+        height={16}
+        priority
+      />
+    </main>
+  );
+}
